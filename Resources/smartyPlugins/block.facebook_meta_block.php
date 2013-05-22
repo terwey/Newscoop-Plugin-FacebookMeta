b83a9e63debd2657abd791c027aa0f062cac326e
Yorick Terweijden <yorick.terweijden@sourcefabric.org>
2013-05-22 15:58:20 +0200

    Plugin works

--- Resources/smartyPlugins/block.facebook_meta_block.php
+++ Resources/smartyPlugins/block.facebook_meta_block.php
@@ -31,6 +31,20 @@ function smarty_block_facebook_meta_block($params, $content, &$smarty, &$repeat)
 
     $smarty->smarty->loadPlugin('smarty_shared_escape_special_chars');
     $context = $smarty->getTemplateVars('gimme');
+    $uri = $smarty->getTemplateVars('uri');
 
-    return '<!-- Test plugin block -->';
+    $html = '';
+    // $html = print_r($context->article, true);
+    if ($context->article->defined) {
+        $html .= '<meta property="og:title" content="'.$context->article->name.'" />'."\n";
+        $html .= '<meta property="og:type" content="article" />'."\n";
+        $html .= '<meta property="og:url" content="http:/'.$context->publication->site. smarty_function_uri($params, $smarty) .'" />'."\n";
+        $html .= '<meta property="og:site_name" content="'. $context->publication->name .'" />'."\n";
+        $html .= '<meta property="og:description" content="'.strip_tags($context->article->deck).'" />'."\n";
+        if ($context->article->image->imageurl) {
+            $html .= '<meta property="og:image" content="'. $context->article->image->imageurl .'" />'."\n";
+        }
+    }
+
+    return $html;
 }
